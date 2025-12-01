121fe7b96cbfcca94b659158c5d64216fddcc27a
Tobias Bühlmann <77664+tbuehlmann@users.noreply.github.com>
2025-12-01 10:07:58 +0100

    Add 2025 day 1

---
+++ 2025/1/2.rb
@@ -0,0 +1,24 @@
+input = File.readlines("input.txt").map(&:strip)
+
+position = 50
+zeros_encountered = 0
+
+input.each do |rotation|
+  direction = rotation[0]
+  clicks = rotation[1..-1].to_i
+
+  clicks.times do
+    case direction
+    when "R"
+      position += 1
+    when "L"
+      position -= 1
+    end
+
+    position = position % 100
+
+    zeros_encountered += 1 if position.zero?
+  end
+end
+
+puts zeros_encountered
